439b9cdc92cbe0dd478b973acaedc9f0d669dae0
Kelemen <503023779@logon.ds.ge.com>
2018-04-30 18:54:27 +0200

    structure - Extend html item selector, move addSelector logic into it

--- src/main/java/com/keli/crawler/core/api/selector/item/HtmlItemSelector.java
+++ src/main/java/com/keli/crawler/core/api/selector/item/HtmlItemSelector.java
@@ -1,12 +1,26 @@
 package com.keli.crawler.core.api.selector.item;
 
-import lombok.AllArgsConstructor;
+import com.keli.crawler.core.api.selector.field.FieldSelector;
+import com.keli.crawler.core.api.service.validator.QueryValidator;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class HtmlItemSelector implements ItemSelector {
 
   private Class<?> classType;
   private String tagCssQuery;
+  private List<FieldSelector> selectors;
+
+  public HtmlItemSelector(Class<?> classType, String tagCssQuery) {
+    this.classType = classType;
+    this.tagCssQuery = tagCssQuery;
+    this.selectors = new ArrayList<>();
+  }
+
+  public void addSelector(FieldSelector fieldSelector) {
+    QueryValidator.validateQuery(fieldSelector.getCssQuery());
+    selectors.add(fieldSelector);
+  }
 }
