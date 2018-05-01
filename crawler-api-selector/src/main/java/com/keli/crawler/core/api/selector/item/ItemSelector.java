f78db0b12c4206ea5dd5305aacdf74d92927457c
Kelemen <503023779@logon.ds.ge.com>
2018-05-01 12:21:35 +0200

    pagination - Refactor ItemSelector, use abstract class, because it stores the fields

--- crawler-api-selector/src/main/java/com/keli/crawler/core/api/selector/item/ItemSelector.java
+++ crawler-api-selector/src/main/java/com/keli/crawler/core/api/selector/item/ItemSelector.java
@@ -1,5 +1,32 @@
 package com.keli.crawler.core.api.selector.item;
 
-public interface ItemSelector {
+import com.keli.crawler.core.api.selector.field.FieldSelector;
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class ItemSelector {
+
+  private Class<?> classType;
+  private String tagCssQuery;
+  private List<FieldSelector> selectors;
+
+  ItemSelector(Class<?> classType, String tagCssQuery) {
+    this.classType = classType;
+    this.tagCssQuery = tagCssQuery;
+    this.selectors = new ArrayList<>();
+  }
+
+  public abstract void addSelector(FieldSelector fieldSelector);
+
+  public Class<?> getClassType() {
+    return classType;
+  }
+
+  public String getTagCssQuery() {
+    return tagCssQuery;
+  }
+
+  public List<FieldSelector> getSelectors() {
+    return selectors;
+  }
 }
