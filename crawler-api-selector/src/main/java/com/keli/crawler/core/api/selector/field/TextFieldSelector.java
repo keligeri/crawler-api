98d2785012b499c0aa5c625e22641ef30d85968c
Kelemen <503023779@logon.ds.ge.com>
2018-05-04 19:25:14 +0200

    pagination - Delete jsoup as root dependency, use only in core module
    Because selectors don't have to know about crawler implementation

--- crawler-api-selector/src/main/java/com/keli/crawler/core/api/selector/field/TextFieldSelector.java
+++ crawler-api-selector/src/main/java/com/keli/crawler/core/api/selector/field/TextFieldSelector.java
@@ -1,14 +1,8 @@
 package com.keli.crawler.core.api.selector.field;
 
-import org.jsoup.nodes.Element;
-
-public class TextFieldSelector extends FieldSelector<String> {
+public class TextFieldSelector extends FieldSelector {
 
   public TextFieldSelector(String fieldName, String cssQuery) {
     super(fieldName, cssQuery);
-  }
-
-  public String execute(Element element) {
-    return element.select(cssQuery).text();
   }
 }
